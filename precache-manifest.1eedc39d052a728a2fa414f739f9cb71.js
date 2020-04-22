09334628f4dfabfe39780ba9d97b05e2c38b92e7
Roma <roman.mykhalchuk@yahoo.com>
2020-04-22 18:40:55 +0300

    Updates

---
+++ precache-manifest.1eedc39d052a728a2fa414f739f9cb71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad8188d43434a1ac5cf670d934d269f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "26d1661a212078421fa6",
+    "url": "./static/js/2.b2110f2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b2110f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22333e2a58632fd2d821",
+    "url": "./static/js/main.c0020f1a.chunk.js"
+  },
+  {
+    "revision": "9a40466c9e3fec559fa3",
+    "url": "./static/js/runtime-main.6a591e0b.js"
+  }
+]);
